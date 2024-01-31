a2c0ed94643ae0430b3d54419959426b01c3d726
Aaron Davis <aaron@ripemetrics.com>
2024-01-30 17:57:15 -0800

    install shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-red-500">
-      <h1>Hello Auth</h1>
+      <Button className="text-red-500">
+        hello
+
+        </Button>
     </div>
   );
 }
