d2f2bf000096a8cc2e5f62dc7e5072607191fbdb
Aaron Davis <aaron@ripemetrics.com>
2024-02-06 21:04:49 -0800

    Update import statements and fix error messages

--- src/lib/actions/register.ts
+++ src/lib/actions/register.ts
@@ -1,19 +1,19 @@
-"use server";
+'use server';
 
-import * as z from "zod";
-import bcrypt from "bcryptjs";
+import * as z from 'zod';
+import bcrypt from 'bcryptjs';
 
-import { db } from "@/lib/utils/db";
-import { RegisterSchema } from "@/lib/schemas";
-import { getUserByEmail } from "@/lib/data/user";
-import { sendVerificationEmail } from "@/lib/utils/mail";
-import { generateVerificationToken } from "@/lib/utils/tokens";
+import { db } from '@/lib/utils/db';
+import { RegisterSchema } from '@/lib/schemas';
+import { getUserByEmail } from '@/lib/data/user';
+import { sendVerificationEmail } from '@/lib/utils/mail';
+import { generateVerificationToken } from '@/lib/utils/tokens';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: "Invalid fields!" };
+    return { error: 'Invalid fields!' };
   }
 
   const { email, password, name } = validatedFields.data;
@@ -22,22 +22,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
-    return { error: "Email already in use!" };
+    return { error: 'Email already in use!' };
   }
 
   await db.user.create({
     data: {
       name,
       email,
-      password: hashedPassword,
-    },
+      password: hashedPassword
+    }
   });
 
-  const verificationToken = await generateVerificationToken(email);
-  await sendVerificationEmail(
-    verificationToken.email,
-    verificationToken.token,
-  );
+  //   const verificationToken = await generateVerificationToken(email);
+  //   await sendVerificationEmail(
+  //     verificationToken.email,
+  //     verificationToken.token,
+  //   );
 
-  return { success: "Confirmation email sent!" };
+  return { success: 'User Created!' };
 };
