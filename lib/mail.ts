ab15c6b3531066da24b2cf8de739e76bdd85ad66
Aaron Davis <aaron@ripemetrics.com>
2024-02-29 09:57:16 -0800

    Add NEXT_PUBLIC_FROM_EMAIL to .env.example and use it in mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,6 +4,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
+const fromEmail = process.env.NEXT_PUBLIC_FROM_EMAIL;
+
 // send a password reset email
 export const sendPasswordResetEmail = async (
     email: string,
@@ -12,7 +14,7 @@ export const sendPasswordResetEmail = async (
     const resetLink = `${domain}/auth/new-password?token=${token}`
 
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: `${fromEmail}`,
       to: email,
       subject: "Reset your password",
       html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
@@ -25,7 +27,7 @@ export const sendPasswordResetEmail = async (
     token: string
   ) => {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: `${fromEmail}`,
       to: email,
       subject: "2FA Code",
       html: `<p>Your 2FA code: ${token}</p>`
@@ -42,7 +44,7 @@ export const sendVerificationEmail = async (
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `${fromEmail}`,
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
