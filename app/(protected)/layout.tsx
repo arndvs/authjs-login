871a182bf40f78b69ed1f2a4ddb4b1ffd1832a46
Aaron Davis <aaron@ripemetrics.com>
2024-03-03 10:35:21 -0800

    Update background colors in layout components

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -5,12 +5,12 @@ interface ProtectedLayoutProps {
 };
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
-  return ( 
-    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+  return (
+    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-green-400 to-green-800">
       <Navbar />
       {children}
     </div>
    );
 }
- 
+
 export default ProtectedLayout;
